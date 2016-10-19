5c6fb0da77cad8407c709d9e5c722a3384ee84ee
Raphael Barbo <rsbarbo@gmail.com>
2016-10-19 11:33:02 -0600

    update gem file with faker for factory girl

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,4 +1,12 @@
 FactoryGirl.define do
   factory :user do
+    first_name: Faker::Name.first_name
+    last_name:
+    email_address:
+    password_digest:
+    phone_number:
+    description:
+    picture_url:
+    status:
   end
 end
