b2dd62596fcfa8c77d09c708f032ebb768f3560d
Raphael Barbo <rsbarbo@gmail.com>
2016-10-19 14:28:29 -0600

    update application controller with current_user and helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name,
+                                 :last_name,
+                                 :email_address,
+                                 :password,
+                                 :phone_number,
+                                 :description,
+                                 :picture_url,
+                                 :status)
+  end
 end
