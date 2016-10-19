c24f2787d20cc5ee207e1d368d1473d017fa57f6
Raphael Barbo <rsbarbo@gmail.com>
2016-10-18 21:59:45 -0600

    all tests are passing, still need to create user to confirm it is logged in with factory_girl

--- spec/features/any_visitor_user_can_visit_the_about_page_spec.rb
+++ spec/features/any_visitor_user_can_visit_the_about_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "any visitor/user can visit the About page" do
   scenario "user can see visit about page" do
     #as a user (logged in)
-    
+
     #when I click link "About"
     visit '/about'
 
@@ -11,7 +11,7 @@ RSpec.feature "any visitor/user can visit the About page" do
     expect(current_path).to eq(about_path)
 
     #and I should see "Created by: David Davydov, Nate Anderson, Raphael Barbo, and Brian Heim"
-    expect(page).to have_content("Created by: David Davydov, Nate Anderson, Raphael Barbo, and Brian Heim")
+    expect(page).to have_content("Created by: Brian Heim, David Davydov, Nate Anderson, and Raphael Barbo")
   end
 
   scenario "visitor can see visit about page" do
@@ -23,6 +23,6 @@ RSpec.feature "any visitor/user can visit the About page" do
     expect(current_path).to eq(about_path)
 
     #and I should see "Created by: David Davydov, Nate Anderson, Raphael Barbo, and Brian Heim"
-    expect(page).to have_content("Created by: David Davydov, Nate Anderson, Raphael Barbo, and Brian Heim")
+    expect(page).to have_content("Created by: Brian Heim, David Davydov, Nate Anderson, and Raphael Barbo")
   end
 end
