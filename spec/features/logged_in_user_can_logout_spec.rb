71c0827dd467539d59e9ed7f44a42d4005dea8a6
Raphael Barbo <rsbarbo@gmail.com>
2016-10-19 15:46:21 -0600

    refactored and clean up some of the files

--- spec/features/logged_in_user_can_logout_spec.rb
+++ spec/features/logged_in_user_can_logout_spec.rb
@@ -2,21 +2,33 @@ require 'rails_helper'
 
 RSpec.feature "User can logout" do
   scenario "after they have logged in" do
-    # As a logged in user`
+    # As a registered user
     user = create(:user)
+
+    #When I visit the homepage
     visit '/'
+
+    #And click on login
     click_on "Login"
+
+    #And I fill out the login form
     fill_in "Email", with: user.email_address
     fill_in "Password", with: user.password
+
+    #And and I click on log in button
     click_button "Login"
+
     # I should see a link on the navbar "Logout"
     within(".navbar") do
       expect(page).to have_content("Logout")
     end
+
     # When I click on "Logout"
     click_on "Logout"
+
     # I should expect to be directed to the homepage
     expect(current_path).to eq("/")
+
     # I expect tp not see my first name, trips, or messages in navbar
     within(".navbar") do
       expect(page).to_not have_content("#{user.first_name}")
@@ -24,6 +36,7 @@ RSpec.feature "User can logout" do
       expect(page).to_not have_content("Messages")
       expect(page).to_not have_content("Logout")
     end
+
     # I expect to see links "login" "sign up" and "about"
     within(".navbar") do
       expect(page).to have_content("Login")
