661e211fb8d150e2f7861c07e246f96c5678e5fe
Raphael Barbo <rsbarbo@gmail.com>
2016-10-19 14:29:36 -0600

    update route with dashboard to redirect the user to their page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'about', to: 'about#index'
   get 'signup', to: 'users#new'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
-
+  get 'user_dashboard', to: 'dashboard#index'
 end
