cec280a5b2e81a6f71555d49b5fc2f5817350cbe
Raphael Barbo <rsbarbo@gmail.com>
2016-10-20 12:08:39 -0600

    update sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email_address: params[:session][:email_address])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_dashboard_path
+      redirect_to dashboard_path
     else
       flash.now[:danger] = "Email and/or Password is invalid, please try again."
       render :new
