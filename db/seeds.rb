9ec82b73d644e6494f751d6b4605dc5b30614dfe
David Davydov <kctrlv@gmail.com>
2016-10-20 16:02:46 -0600

    seed file has users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-
-
 
 class Seed
   def self.start
     generate_roles
+    generate_users
   end
 
   def self.generate_roles
@@ -21,6 +13,43 @@ class Seed
     Role.create(name: "host")
     Role.create(name: "admin")
     puts "Roles created for traveler, host, admin"
+  end
+
+  def self.generate_users
+    User.delete_all
+    inactive =  User.create(first_name: 'Inactive',
+                last_name:  'Johnson',
+                email_address: 'inactive@restfulstay.com',
+                password: 'inactive',
+                status: 'inactive')
+    puts "inactive user created"
+
+    traveler =  User.create(first_name: 'Traveler',
+                last_name:  'Johnson',
+                email_address: 'traveler@restfulstay.com',
+                password: 'traveler',
+                status: 'active')
+    traveler.roles << Role.find_by(name: 'traveler')
+    puts "traveler user created"
+
+    host =      User.create(first_name: 'Host',
+                last_name:  'Johnson',
+                email_address: 'host@restfulstay.com',
+                password: 'host',
+                status: 'active')
+    host.roles << Role.find_by(name: 'traveler')
+    host.roles << Role.find_by(name: 'host')
+    puts "host user created"
+
+    admin =     User.create(first_name: 'Admin',
+                last_name:  'Johnson',
+                email_address: 'admin@restfulstay.com',
+                password: 'admin',
+                status: 'active')
+    host.roles << Role.find_by(name: 'traveler')
+    host.roles << Role.find_by(name: 'host')
+    host.roles << Role.find_by(name: 'admin')
+    puts "admin user created"
   end
 end
 
