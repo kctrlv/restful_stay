76234caaaa8d37dde5279d6f9563888fe4f5a56f
Raphael Barbo <rsbarbo@gmail.com>
2016-10-19 20:17:36 -0600

    setup twilio service

---
+++ app/services/twilio_service.rb
@@ -0,0 +1,30 @@
+require 'net/http'
+
+class TwilioService
+
+  def send_confirmation_code
+      uri = URI("https://api.twilio.com/2010-04-01/Accounts/#{ENV['twilio_account_sid']}/Messages.json")
+      connection.get uri
+  end
+
+  def self.generate
+    rand(100000...999999).to_s
+  end
+
+  def self.connection
+    params = {Username: ENV['twilio_account_sid'],
+              Password: ENV['twilio_auth_token'],
+              To: ENV['to_number'] ,
+              From: ENV['from_number'],
+              Body: "Hello from RESTfulStay, Enter your code #{TwilioService.generate}"
+              }
+    uri.query = URI.encode_www_form(params)
+
+    res = Net::HTTP.get_response(uri)
+    puts parse(res.body) if res.is_a?(Net::HTTP)
+  end
+
+  def parse(response)
+    JSON.parse(response, symbolize_names: true)
+  end
+end
