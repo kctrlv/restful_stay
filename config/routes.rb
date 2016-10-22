8c785a7f122157655dc2a86de3f7997709e98700
David Davydov <kctrlv@gmail.com>
2016-10-22 15:54:09 -0600

    fix failing tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :listings, only: [:index, :new, :create, :show]
+  resources :cities, param: :name, only: [:show] do
+    resources :listings, only: [:index]
+  end
+
 
   resources :userroles, only: [:create]
 
