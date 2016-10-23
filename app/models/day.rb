6d3fa443fb381ac4c7036ac9d2bcbed6541015f3
David Davydov <kctrlv@gmail.com>
2016-10-23 10:51:43 -0600

    add future_days scope to Day

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,5 +1,6 @@
 class Day < ApplicationRecord
   validates_presence_of :date
+  scope :future_days, -> { where("date >= :date", date: Date.today).order(:date) }
 
   has_many :listing_days
   has_many :listings, through: :listing_days
