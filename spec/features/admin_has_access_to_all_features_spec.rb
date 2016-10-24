2e944d4fffd1b1ac957effa5d5fafae18c6dc13c
Nate Anderson <curtislowe10@gmail.com>
2016-10-24 17:20:46 -0600

    update test

--- spec/features/admin_has_access_to_all_features_spec.rb
+++ spec/features/admin_has_access_to_all_features_spec.rb
@@ -7,12 +7,12 @@ RSpec.feature "Admin can see and manipulate all features of the site" do
   
     # As an admin, I will log in and expect to be redirected to admin dashboard
     visit '/login'
-    
+    # And I enter my information
     fill_in "Email", with: admin.email_address
     fill_in "Password", with: admin.password
     #And and I click on log in button
     click_button "Login"
-    
+    #I expect to be in the 'admin/dashboard'
     expect(current_path).to eq('/admin/dashboard')
   end
 end
