dd1fd480fb4ce98b0ed056b872b098588beeea25
Nate Anderson <curtislowe10@gmail.com>
2016-10-24 17:27:44 -0600

    create test for admin dashboard

--- spec/features/admin_has_access_to_all_features_spec.rb
+++ spec/features/admin_has_access_to_all_features_spec.rb
@@ -15,4 +15,21 @@ RSpec.feature "Admin can see and manipulate all features of the site" do
     #I expect to be in the 'admin/dashboard'
     expect(current_path).to eq('/admin/dashboard')
   end
+  
+  scenario "when admin logs in, we expect page to have Admin Info, Listings, Users" do
+    admin = create(:user, status: "active")
+    admin.roles << Role.find(3)
+    # As an admin, I will log in and expect to be redirected to admin dashboard
+    login_user(admin)
+    #I expect to be in the 'admin/dashboard'
+    expect(current_path).to eq('/admin/dashboard')
+    # I expect page to have the following contents : Admin info, listings, users, create city
+    expect(page).to have_content("Admin Dashboard")
+    expect(page).to have_content("Manage Users")
+    expect(page).to have_content("Manage Listings")
+    expect(page).to have_content("Manage Cities")
+  end
+  
+    xscenario "admin should be able to create a new city" do
+  end
 end
