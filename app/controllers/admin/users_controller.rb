e3120a4c3b15decb1b1c3c315c6c395e5a20e3e7
Raphael Barbo <rsbarbo@gmail.com>
2016-10-24 20:07:01 -0600

    update users controller admin and users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,18 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def edit
+    @user = User.find(params[:user_id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    if @user.update_attribute(:status, params[:user][:status])
+      redirect_to admin_manage_users_path
+    else
+      flash.now[:danger] = "Please make sure you have all the info needed."
+      redirect_to admin_edit_user_path
+    end
+  end
 end
