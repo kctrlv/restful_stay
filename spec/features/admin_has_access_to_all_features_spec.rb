6008ad948aeed00c70c0e11542b2aeeef48cdf1a
Raphael Barbo <rsbarbo@gmail.com>
2016-10-24 20:43:34 -0600

    update permission and admin test

--- spec/features/admin_has_access_to_all_features_spec.rb
+++ spec/features/admin_has_access_to_all_features_spec.rb
@@ -30,24 +30,24 @@ RSpec.feature "Admin can see and manipulate all features of the site" do
     expect(page).to have_content("Manage Cities")
   end
 
-    scenario "admin should be able to go to Manage Users page" do
-      admin = create(:user, status: "active")
-      admin.roles << Role.find(3)
-      # As an admin, I will log in and expect to be redirected to admin dashboard
-      login_user(admin)
-      #I expect to be in the 'admin/dashboard'
-      expect(current_path).to eq('/admin/dashboard')
-      # When I click Link Manager Users
-      click_link "Manage Users"
-      # I expect to be redirected to '/admin/dashboard/manage_users'
-      expect(current_path).to eq('/admin/dashboard/manage_users')
-      # I expect page to have content Manage Users
-      expect(page).to have_content("Manage Users")
-      # I expect to see a list of all users and their status
-      expect(page).to have_content("Edit")
-      expect(page).to have_content("First name")
-      expect(page).to have_content("Last name")
-      expect(page).to have_content("Status")
+  scenario "admin should be able to go to Manage Users page" do
+    admin = create(:user, status: "active")
+    admin.roles << Role.find(3)
+    # As an admin, I will log in and expect to be redirected to admin dashboard
+    login_user(admin)
+    #I expect to be in the 'admin/dashboard'
+    expect(current_path).to eq('/admin/dashboard')
+    # When I click Link Manager Users
+    click_link "Manage Users"
+    # I expect to be redirected to '/admin/dashboard/manage_users'
+    expect(current_path).to eq('/admin/dashboard/manage_users')
+    # I expect page to have content Manage Users
+    expect(page).to have_content("Manage Users")
+    # I expect to see a list of all users and their status
+    expect(page).to have_content("Edit")
+    expect(page).to have_content("First name")
+    expect(page).to have_content("Last name")
+    expect(page).to have_content("Status")
   end
 
   scenario "admin be able to see a specific user page" do
@@ -66,6 +66,33 @@ RSpec.feature "Admin can see and manipulate all features of the site" do
     # When I click on the user id I should be redirected to admin user show page
     click_link "1"
     expect(current_path).to eq('/admin/dashboard/manage_users/1/edit')
-end
+  end
 
+  scenario "admin update status of specific user" do
+    admin = create(:user, status: "active")
+    admin.roles << Role.find(3)
+    user_1 = create(:user, first_name: "Raphael", last_name: "Barbo",  status: 'active')
+    # As an admin, I will log in and expect to be redirected to admin dashboard
+    login_user(admin)
+    #I expect to be in the 'admin/dashboard'
+    expect(current_path).to eq('/admin/dashboard')
+    # When I click Link Manager Users
+    click_link "Manage Users"
+    # I expect to be redirected to '/admin/dashboard/manage_users'
+    expect(current_path).to eq('/admin/dashboard/manage_users')
+    # I expect page to have content Manage Users
+    expect(page).to have_content("Manage Users")
+    # When I click on the user id I should be redirected to admin user show page
+    click_link "#{user_1.id}"
+    expect(current_path).to eq("/admin/dashboard/manage_users/#{user_1.id}/edit")
+    # And I update user status to suspended
+    select "suspended", from: "user_status"
+    save_and_open_page
+    # And I click on "Update User Profile"
+    click_button "Update User Profile"
+    # I expect to be redirected to admin manage user dashboard
+    expect(current_path).to eq('/admin/dashboard/manage_users')
+    # I expect user_1 status to equal suspended
+    expect(user_1.status).to eq('suspended')
+  end
 end
