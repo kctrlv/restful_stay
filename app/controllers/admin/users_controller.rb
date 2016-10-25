79c634c9e317b1515b9eedc09c36a70117f66796
Raphael Barbo <rsbarbo@gmail.com>
2016-10-25 10:59:58 -0600

    moved update attribute to a private and secude method

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,11 +9,16 @@ class Admin::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:user_id])
-    if @user.update_attribute(:status, params[:user][:status])
+    if status_change_params(@user)
       redirect_to admin_manage_users_path
     else
       flash.now[:danger] = "Please make sure you have all the info needed."
       redirect_to admin_edit_user_path
     end
+  end
+
+  private
+  def status_change_params(user)
+    user.update_attribute(:status, params[:user][:status])
   end
 end
