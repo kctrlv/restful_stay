18560d90d05183cf8fa2da2b1b7cd784550a400a
David Davydov <kctrlv@gmail.com>
2016-10-25 14:16:32 -0600

    more bugfixes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,7 +8,6 @@ class TripsController < ApplicationController
   end
 
   def create
-    byebug
     return wrong_dates if trip_params[:checkin].to_i > trip_params[:checkout].to_i
     @trip = Trip.new(listing: Listing.find(trip_params[:listing_id]),
                      guest: current_user,
