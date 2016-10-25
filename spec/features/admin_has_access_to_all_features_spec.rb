a5d05646c91bd05dd56d300540bfec7c2b1dca69
Raphael Barbo <rsbarbo@gmail.com>
2016-10-24 20:15:17 -0600

    update test to cover all edge cases

--- spec/features/admin_has_access_to_all_features_spec.rb
+++ spec/features/admin_has_access_to_all_features_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Admin can see and manipulate all features of the site" do
   scenario "admin can sign in and visit admin dashboard" do
     admin = create(:user, status: "active")
     admin.roles << Role.find(3)
-  
+
     # As an admin, I will log in and expect to be redirected to admin dashboard
     visit '/login'
     # And I enter my information
@@ -15,7 +15,7 @@ RSpec.feature "Admin can see and manipulate all features of the site" do
     #I expect to be in the 'admin/dashboard'
     expect(current_path).to eq('/admin/dashboard')
   end
-  
+
   scenario "when admin logs in, we expect page to have Admin Info, Listings, Users" do
     admin = create(:user, status: "active")
     admin.roles << Role.find(3)
@@ -29,8 +29,8 @@ RSpec.feature "Admin can see and manipulate all features of the site" do
     expect(page).to have_content("Manage Listings")
     expect(page).to have_content("Manage Cities")
   end
-  
-    scenario "admin should be able to create a new city" do
+
+    scenario "admin should be able to go to Manage Users page" do
       admin = create(:user, status: "active")
       admin.roles << Role.find(3)
       # As an admin, I will log in and expect to be redirected to admin dashboard
@@ -44,9 +44,28 @@ RSpec.feature "Admin can see and manipulate all features of the site" do
       # I expect page to have content Manage Users
       expect(page).to have_content("Manage Users")
       # I expect to see a list of all users and their status
+      expect(page).to have_content("Edit")
       expect(page).to have_content("First name")
       expect(page).to have_content("Last name")
       expect(page).to have_content("Status")
-      #expect(page).to selector(to manipulate that user)
   end
+
+  scenario "admin be able to see a specific user page" do
+    admin = create(:user, status: "active")
+    admin.roles << Role.find(3)
+    # As an admin, I will log in and expect to be redirected to admin dashboard
+    login_user(admin)
+    #I expect to be in the 'admin/dashboard'
+    expect(current_path).to eq('/admin/dashboard')
+    # When I click Link Manager Users
+    click_link "Manage Users"
+    # I expect to be redirected to '/admin/dashboard/manage_users'
+    expect(current_path).to eq('/admin/dashboard/manage_users')
+    # I expect page to have content Manage Users
+    expect(page).to have_content("Manage Users")
+    # When I click on the user id I should be redirected to admin user show page
+    click_link "1"
+    expect(current_path).to eq('/admin/dashboard/manage_users/1/edit')
+end
+
 end
