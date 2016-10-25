19cbb490dd25352fa06db3ce51ce0e43ecb06a9d
Nate Anderson <curtislowe10@gmail.com>
2016-10-25 14:26:39 -0600

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ApplicationRecord
   def inactive?
     status == 'inactive'
   end
+  
+  def suspended?
+    status == 'suspended'
+  end
 
   def activate
     update_attribute(:status, "active")
