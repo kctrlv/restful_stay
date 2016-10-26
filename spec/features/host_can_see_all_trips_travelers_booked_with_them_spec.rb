a27e9752dc3a741526958254d5c2214f49571500
Raphael Barbo <rsbarbo@gmail.com>
2016-10-25 21:23:36 -0600

    update test to cover edge cases

--- spec/features/host_can_see_all_trips_travelers_booked_with_them_spec.rb
+++ spec/features/host_can_see_all_trips_travelers_booked_with_them_spec.rb
@@ -7,52 +7,36 @@ RSpec.feature "Host can see all trips travelers booked with them" do
 
     visit reservations_path
 
-    expect(current_path).to eq('something')
+    expect(current_path).to eq('/reservations')
 
+    expect(page).to have_content("Guest Name")
+    expect(page).to have_content("Traveler Johnson")
+
+    expect(page).to have_content("Trip Start Date")
+    expect(page).to have_content("2016-11-05")
+
+    expect(page).to have_content("Trip End Date	")
+    expect(page).to have_content("2016-11-07	")
+
+    expect(page).to have_content("Listing Details")
+    expect(page).to have_content("Cool Room")
+  end
+
+  scenario "host can see guest profile page" do
+    login_as_denverhost
+    book_trip_for_listing
+
+    visit reservations_path
+
+    expect(current_path).to eq('/reservations')
+
+    expect(page).to have_content("Guest Name")
+    expect(page).to have_content("Traveler Johnson")
+
+    click_link "Traveler Johnson"
+
+    expect(current_path).to eq("/profile/2")
+
+    expect(page).to have_content("Traveler Johnson's Profile")
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# user = User.find_by(first_name: "denverhost")
-# login(user, 'denverhost')
-#
-# click_link "Listings"
-#
-# expect(current_path).to eq('/listings')
-#
-# click_link "Create Listing"
-#
-# expect(current_path).to eq('/listings/new')
-#
-# fill_in "Name", with: "My Listing"
-# fill_in "Description", with: "Testing"
-# fill_in "Price per night", with: "19.99"
-# select "2016-10-26", from: "listing[start_date]"
-# select "2016-10-29", from: "listing[end_date]"
-#
-# click_button "Create Listing"
-#
-# visit '/dashboard'
-#
-# click_link "Reservations"
-#
-# expect(current_path).to eq("/reservations")
-#
-# expect(page).to have_content("2016-11-05")
-# expect(page).to have_content("2016-11-07")
-# expect(page).to have_content("Traveler")
