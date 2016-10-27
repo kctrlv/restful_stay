a2ab1ece733898b2f14dda181586f299cb82c233
Raphael Barbo <rsbarbo@gmail.com>
2016-10-26 18:00:29 -0600

    update application controller to null_exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
   def render_404
     render :file => "#{Rails.root}/public/404.html",  :status => 404
   end
-  
+
   def path_based_on_role
     if current_user.admin?
       redirect_to admin_dashboard_path
