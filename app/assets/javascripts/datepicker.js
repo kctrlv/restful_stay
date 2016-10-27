9c8f55b036a545fdcf9a57c69aa0f68c242143e3
Raphael Barbo <rsbarbo@gmail.com>
2016-10-26 18:37:41 -0600

    update api v1 controller with correct function, and update datepick js

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -9,8 +9,9 @@ $(document).ready(function() {
   });
 
   function fetchDates() {
+    var listingId = getID();
     $.ajax({
-      url: `http://localhost:3000/api/v1/availabledates/2`,
+      url: `http://localhost:3000/api/v1/availabledates/${listingId}`,
       type: 'GET',
       success: (response) => {
         booking = response
@@ -21,7 +22,7 @@ $(document).ready(function() {
 
   function checkAvailability(date) {
     var availableDays = booking.map(function(day) {
-      return day.date
+      return day
     })
     var y = date.getFullYear();
     var m = date.getMonth();
@@ -36,6 +37,6 @@ $(document).ready(function() {
   }
 
   function getID() {
-    return window.location.pathname.split('-').slice(-1).toString();
+    return window.location.pathname.split('/').slice(-1).toString();
   }
 })
